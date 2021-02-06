37752ce0da257ba9b5753767501842ba41345fc6
Mauro OLSEN <mauro123olsen@gmail.com>
2021-02-06 12:51:33 -0300

    upload and validate

--- upload.php
+++ upload.php
@@ -1,14 +1,43 @@
 <?php
-    if(isset($_POST['submit'])){
-        $uploadPath = '/uploads/';
-        $fileName = (isset($_FILES['file'])) ? basename($_FILES['file']['name']) : '';
-        $filePath = $uploadPath . $fileName;
-        var_dump($fileName);
-        var_dump($filePath);
-        if(var_dump(move_uploaded_file($fileName, $filePath))){
-            echo 'Archivo subido correctamente...';
-        }else{
-            echo 'El archivo no se pudo subir correctamente...';
-        }
-
+function subir($fileName, $filePath)
+{
+    $message = false;
+    if (move_uploaded_file($fileName, $filePath)) {
+        $message = true;
     }
+    return $message;
+}
+
+function validarArchivo($fileName, $filePath)
+{
+    $ret = true;
+    if ($fileName == '' || basename($filePath) == '') {
+        $ret = false;
+    } else {
+        if (file_exists($filePath)) {
+            $ret = false;
+        }
+    }
+    return $ret;
+}
+
+function validarPath($path){
+    if(!file_exists($path))
+        mkdir($path);
+}
+
+if (isset($_POST['submit'])) {
+    $uploadPath = 'uploads\\';
+    validarPath($uploadPath);
+    $fileName = (isset($_FILES['file'])) ? ($_FILES['file']['tmp_name']) : '';
+    $targetfileName = (isset($_FILES['file'])) ? basename($_FILES['file']['name']) : '';
+    $filePath = $uploadPath . $targetfileName;
+    $message = false;
+
+    if (validarArchivo($fileName, $filePath)) {
+        $message = subir($fileName, $filePath);
+    }
+
+    $message = ($message) ? 'true' : 'false';
+    header('Location: index.php?message=' . strval($message));
+}
