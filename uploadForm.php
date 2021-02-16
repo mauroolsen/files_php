b8024d2078418c1a2959989b1d8908913bacd112
Mauro OLSEN <mauro123olsen@gmail.com>
2021-02-15 22:06:35 -0300

    Update uploadForm.php

--- uploadForm.php
+++ uploadForm.php
@@ -1,7 +1,22 @@
+<style>
+.visuallyhidden {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+}
+</style>
 <form class="form m-2" action="action/upload.php" method="POST" enctype="multipart/form-data">
-  <div class="form-group">
-    <label for="file">Imágenes en formato .png/.jpeg</label>
-    <input class="form-control-files" type="file" id="file" name="file">
-  </div>
-  <button class="btn btn-primary" type="submit" name="submit">UPLOAD</button>
+    <input type="file" name="file" id="file-input" onchange="submit()" class="visuallyhidden">
 </form>
+<button class="file-upload btn btn-primary" onclick="desvio()">UPLOAD</button>
+
+<script>
+function desvio() {
+  document.getElementById('file-input').click();
+}
+</script>
